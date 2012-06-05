99ec44b917311bdc81b74c79b4e604031677f800
Ben Herila <bherila@bherila.net>
2012-06-04 18:41:41 -0700

    moved to mvc3 dinamico

---
+++ src/www/Dinamico/Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using N2.Edit.FileSystem;
+using System.Web.Routing;
+using N2.Web.Mvc.Html;
+using Dinamico.Models;
+using N2.Definitions.Runtime;
+
+namespace Dinamico.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+		{
+			StringWriter sw = new StringWriter();
+			IFileSystem files = N2.Context.Current.Resolve<IFileSystem>();
+			List<ContentRegistration> expressions = new List<ContentRegistration>();
+			foreach (var file in files.GetFiles("~/Views/DynamicPages/").Where(f => f.Name.EndsWith(".cshtml")))
+			{
+				var cctx = new ControllerContext(ControllerContext.HttpContext, new RouteData(), new ContentPagesController());
+				cctx.RouteData.Values.Add("controller", "DynamicPages");
+				var v = ViewEngines.Engines.FindView(cctx, file.VirtualPath, null);
+
+				if (v.View == null)
+					sw.Write(string.Join(", ", v.SearchedLocations.ToArray()));
+				else
+				{
+					var vdd = new ViewDataDictionary { Model = new ContentPage() };
+					var re = new ContentRegistration();
+					vdd["RegistrationExpression"] = re;
+					v.View.Render(new ViewContext(cctx, v.View, vdd, new TempDataDictionary(), sw), sw);
+					expressions.Add(re);
+				}
+			}
+			return View(expressions);
+		}
+
+    }
+}
---
+++ src/www/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="n2cms_www.MvcApplication" Language="C#" %>
